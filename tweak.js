ce858a609e9b491750720fd748f363b49d70a414
Captain Fantasy <captain@fantasyui.com>
2017-05-30 13:35:05 -0400

    Upgrade to Category Objects

---
+++ tweak.js
@@ -0,0 +1,12 @@
+const db = require('./products.json');
+
+db.data.forEach(product=>{
+  product.categories = [{label:"Web Design"}]
+  product.properties.forEach(property=>{
+    if(property.label === "File Size"){
+      property.text = "~100MB";
+    }
+  });
+});
+
+console.log( JSON.stringify(db, null, '  ') );
